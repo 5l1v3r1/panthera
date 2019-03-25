6440487ed89a68e231b8cfd37eefa424589bc1ed
Haroon Awan <39236904+haroonawanethicalhacker@users.noreply.github.com>
2019-03-25 13:00:03 +0500

    Create dnszonetransfer.pl

---
+++ dnszonetransfer.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use Term::ANSIColor;
+system "clear";
+print color('bold red');
+print "\n\n					   	 	Project: Panthera\n";
+print "\n						        Coder: Haroon Awan\n\n\n";
+print color('bold yellow');
+print "[ + ] Version: 		Open Source Edition 1.0a\n";
+print "[ + ] Contact: 		mrharoonawan\@gmail\.com \n";
+print "[ + ] Environment: 	DNS module, Perl under Kali Linux\n";
+print "[ + ] Github: 		Https://www.github.com/haroonawanethicalhacker\n";
+print "[ + ] Design Scheme: 	DNS zone transfers, AXFR\n";
+print "[ + ] Usage: 		$0 url.com\n\n";
+print color('reset');
+print color('bold green');
+$site="$ARGV[0]";
+
+print "[ + ] Enter url.com ... : "; 
+my $url = <STDIN>;
+chomp $url;
+my $sys = system("nslookup -query=ns '$url'");
+print "[ + ] Enter name servers ... : "; 
+my $ns = <STDIN>;
+chomp $ns;
+print "\n[ + ] Contacting target ...\n";
+sleep (2);
+print "\n[ + ] Please wait, starting ...\n";
+print "\n[ + ] Collecting output, it will take a minute or more, depending on the data ...\n\n";
+$done = system("host -t axfr '$url' '$ns'");
+exit;
